8fb6c1bf8ef7163583dc93cd7fca8f78394b8865
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-05 19:03:15 +0800

    PDF encrypt.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Certificate of Attendance generator
 
     $ brew install xhtml2pdf
 
+And install pdftk.
+
 Make pdfs in pdf/, and outputs a slugged csv for Mailchimp
 
     $ ruby generate.rb < file.csv > out.csv
--- generate.rb
+++ generate.rb
@@ -8,19 +8,22 @@ end
 template = File.read('template/index.html')
 output = []
 salt = "manila-js-003-"
+passsalt = "mjs003" # change me
 
 STDIN.read.split("\n").each do |line|
   name, email = line.split(",")
 
   slug = slugify(email, salt)
+  pass = slugify(slug, salt)[0..10]
   html = template.gsub('{{ name }}', name)
-  out  = "pdf/#{slug}.pdf"
+  out  = "#{slug}.pdf"
   tmp  = "template/_tmp.html"
   File.open(tmp, 'w') { |f| f.write html }
 
   # puts "==> #{out} (#{name})"
-  output << [name, email, slug].join(",")
-  system "xhtml2pdf template/_tmp.html #{out} 1>&2"
+  output << [name, email, out, pass].join(",")
+  system "xhtml2pdf template/_tmp.html pdf/_tmp.pdf 1>&2"
+  system "pdftk pdf/_tmp.pdf output pdf/#{out} owner_pw #{pass}_ user_pw #{pass} allow printing"
   File.unlink tmp
 end
 
