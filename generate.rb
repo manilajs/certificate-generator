a3a438ed0264fea1d711767d9be8a4527307a3f7
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-06 16:40:19 +0800

    No passwords.

--- generate.rb
+++ generate.rb
@@ -5,26 +5,27 @@ def slugify(str, salt)
   Digest::SHA1.hexdigest(salt + str)
 end
 
+def xhtml2pdf(input, output)
+  system "xhtml2pdf #{input} #{output} 1>&2"
+end
+
 template = File.read('template/index.html')
 output = []
 salt = "manila-js-003-"
-passsalt = "mjs003" # change me
 urlprefix = "http://manilajs.github.io/certificates/"
 
 STDIN.read.split("\n").each do |line|
   name, email = line.split(",")
 
   slug = slugify(email, salt)
-  pass = slugify(slug, passsalt)[0..10]
   html = template.gsub('{{ name }}', name)
   out  = "#{slug}.pdf"
   tmp  = "template/_tmp.html"
   File.open(tmp, 'w') { |f| f.write html }
 
-  # puts "==> #{out} (#{name})"
-  output << [name, email, urlprefix+out, pass].join(",")
-  system "xhtml2pdf template/_tmp.html pdf/_tmp.pdf 1>&2"
-  system "pdftk pdf/_tmp.pdf output pdf/#{out} owner_pw #{pass}_ user_pw #{pass} allow printing"
+  output << [name, email, urlprefix+out].join(",")
+
+  xhtml2pdf "template/_tmp.html", "pdf/#{out}"
   File.unlink tmp
 end
 
