622d4890a83ae61c1b6d09ae6f17820c351ab86e
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-10 01:38:14 +0800

    Update schema.

--- generate.rb
+++ generate.rb
@@ -12,10 +12,11 @@ end
 template = File.read('template/index.html')
 output = []
 salt = "manila-js-003-"
-urlprefix = "http://manilajs.github.io/certificates/"
 
 STDIN.read.split("\n").each do |line|
-  name, email = line.split(",")
+  lname, fname, email, company = line.split(/[,\t]/)
+
+  name = [fname, lname].join(' ')
 
   slug = slugify(email, salt)
   html = template.gsub('{{ name }}', name)
@@ -23,7 +24,7 @@ STDIN.read.split("\n").each do |line|
   tmp  = "template/_tmp.html"
   File.open(tmp, 'w') { |f| f.write html }
 
-  output << [name, email, urlprefix+out].join(",")
+  output << [lname, fname, email, company, out].join("\t")
 
   xhtml2pdf "template/_tmp.html", "pdf/#{out}"
   File.unlink tmp
